298f2065d858e678e68e1e14e8e74f516477d952
40-PAR-3C <00001093030422sp@al.educacao.sp.gov.br>
2024-11-01 10:40:03 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação que faz muito mais coisa que HTML, mas ela é complicada demais'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é a Torre Eiffel'
+)
+
+criaCartao(
+    'Matemática',
+    'O que é util de se estudar pro enem??',
+    'Pergunta genuína prof, eu n faço ideia'
+)
+
+criaCartao(
+    'Lingua inglesa, só que muito longo pq o carão fica esticado',
+    'Como se diz oi em Inglês?',
+    'Oi em inglês é "aloha!"(Alorrai)'
+)
+
+criaCartao(
+    'Aulas do Alura',
+    'Essa aula foi boa?',
+    'Sim, muito boa... mas podia ter usado esse sistema pra "aventura" né? mó cansativo ficar organizando aqueles textos em HTML, masi fácil por aqui'
+)
